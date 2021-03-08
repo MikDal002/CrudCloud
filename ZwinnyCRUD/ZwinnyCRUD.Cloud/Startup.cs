7d5f27c167052792ef67cd6eb0201d9a33d93c2c
Mikołaj Dalecki <dalecki.mikolaj@gmail.com>
2021-03-09 00:43:02 +0100

    Zmianna projektu na ten, który powinien być (Razor).

--- ZwinnyCRUD/ZwinnyCRUD.Cloud/Startup.cs
+++ ZwinnyCRUD/ZwinnyCRUD.Cloud/Startup.cs
@@ -3,14 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using ZwinnyCRUD.Cloud.Data;
-using Microsoft.EntityFrameworkCore;
 
 namespace ZwinnyCRUD.Cloud
 {
@@ -24,15 +21,9 @@ namespace ZwinnyCRUD.Cloud
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
-
-            services.AddDbContext<ZwinnyCRUDCloudContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ZwinnyCRUDCloudContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,10 +45,11 @@ namespace ZwinnyCRUD.Cloud
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
+                endpoints.MapRazorPages();
             });
         }
     }
