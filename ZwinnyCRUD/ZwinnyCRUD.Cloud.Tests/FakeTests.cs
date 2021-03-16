7666b58c4c2ed11d12c2a4ee7db4eb4a85422644
Karol Appel <karapp002@o365.student.utp.edu.pl>
2021-03-16 19:36:20 +0100

    Merged PR 4: Dodanie testów jednostkowych do CI
    Related work items: #8

---
+++ ZwinnyCRUD/ZwinnyCRUD.Cloud.Tests/FakeTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace ZwinnyCRUD.Cloud.Tests
+{
+    public class FakeTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
