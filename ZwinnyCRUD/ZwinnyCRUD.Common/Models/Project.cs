c67f7130eb55d4e64b9cba62f2adedd7644194d2
Mateusz Lewandowski <42372751+MateuszLewandowski@users.noreply.github.com>
2021-03-17 19:15:24 +0100

    Dodanie podstawowej walidacji create/edit dla modelu Project

--- ZwinnyCRUD/ZwinnyCRUD.Common/Models/Project.cs
+++ ZwinnyCRUD/ZwinnyCRUD.Common/Models/Project.cs
@@ -8,9 +8,16 @@ namespace ZwinnyCRUD.Common.Models
     public class Project
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(31, ErrorMessage = "Tytuł jest zbyt długi.")] 
         public string Title { get; set; }
-        public string Description { get; set; }
         
+        [Required]
+        [StringLength(255, ErrorMessage = "Opis jest zbyt długi.")] 
+        public string Description { get; set; }
+
+        [Required]
         [DataType(DataType.Date)]
         public DateTimeOffset CreationDate {get; set;}
     }
