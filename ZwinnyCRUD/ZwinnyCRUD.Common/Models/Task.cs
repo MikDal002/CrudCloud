ddc3deff476a88968d24a4dc7d6575349e1cc565
Mateusz Lewandowski <42372751+MateuszLewandowski@users.noreply.github.com>
2021-03-20 19:21:54 +0100

    Utworzenie modelu Task

---
+++ ZwinnyCRUD/ZwinnyCRUD.Common/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ZwinnyCRUD.Common.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(31, ErrorMessage = "Tytuł jest zbyt długi.")]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "Opis jest zbyt długi.")]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTimeOffset CreationDate { get; set; }
+    }
+}
