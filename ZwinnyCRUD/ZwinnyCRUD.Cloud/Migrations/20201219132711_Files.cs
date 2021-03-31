2dc258f3e5c8288f8ef92915d3a19848e3babc32
prznad005 <prznad005@o365.student.utp.edu.pl>
2021-03-31 19:30:15 +0200

    Updated 20201219132711_Files.cs

--- ZwinnyCRUD/ZwinnyCRUD.Cloud/Migrations/20201219132711_Files.cs
+++ ZwinnyCRUD/ZwinnyCRUD.Cloud/Migrations/20201219132711_Files.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace ZwinnyCRUD.Cloud.Migrations
 {
@@ -6,18 +7,39 @@ namespace ZwinnyCRUD.Cloud.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "Note",
-                table: "File");
+            migrationBuilder.CreateTable(
+                name: "File",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FilePath = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    SizeinBytes = table.Column<long>(nullable: false),
+                    Uploaded = table.Column<DateTimeOffset>(nullable: false),
+                    ProjectId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_File", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_File_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_File_ProjectId",
+                table: "File",
+                column: "ProjectId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "Note",
-                table: "File",
-                type: "nvarchar(max)",
-                nullable: true);
+            migrationBuilder.DropTable(
+                name: "File");
         }
     }
 }
